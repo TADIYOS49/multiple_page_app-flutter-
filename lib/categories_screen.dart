02c0b7934ba111188a6ff52eab18c9cab1e82d26
Tadiyos Begashaw <tadiyosbanti@gmail.com>
2021-04-19 16:06:27 +0300

    update on the return

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -11,14 +11,14 @@ class CategoryScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
+        children: DUMMY_CATEGORIES.map((item) {
+          return CategoryItem(item.title, item.color);
+        }).toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
-        children: DUMMY_CATEGORIES.map((item) {
-          CategoryItem(item.title, item.color);
-        }).toList(),
       ),
     );
   }
